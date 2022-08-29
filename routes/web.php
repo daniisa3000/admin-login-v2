757f3ff37bd2af345f241c09014c045ad0cbba3d
Daniela <daniisa990@gmail.com>
2022-08-29 17:42:57 -0400

    add routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/users', 'UsersController@index')->middleware(['web','auth']);
+Route::get('/users/create', 'UsersController@create')->middleware(['web','auth']);
+Route::post('/users/store', 'UsersController@store')->middleware(['web','auth']);
+Route::get('/users/edit/{id}', 'UsersController@edit')->middleware(['web','auth']);
+Route::get('/users/{id}', 'UsersController@show')->middleware(['web','auth']);
+Route::put('/users/{id}', 'UsersController@update')->middleware(['web','auth']);
+Route::delete('/users/{id}', 'UsersController@destroy')->middleware(['web','auth']);
+
+Route::get('/roles', 'RolesController@index')->middleware(['web','auth']);
+Route::get('/roles/{id}', 'RolesController@show')->middleware(['web','auth']);
+
+Route::get('/profile', 'ProfileController@show')->middleware(['web','auth']);
+Route::get('/profile/edit', 'ProfileController@edit')->middleware(['web','auth']);
+Route::put('/profile', 'ProfileController@update')->middleware(['web','auth']);
